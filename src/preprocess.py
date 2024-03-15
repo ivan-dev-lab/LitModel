9fa379112d37d418fa2af10bd1663b944012f974
ivan-dev-lab <roalivan4@gmail.com>
2024-03-15 15:34:25 +0500

    написана функция для очистки текста

--- src/preprocess.py
+++ src/preprocess.py
@@ -4,15 +4,29 @@ import os
 import string
 
 # прочтение всех необработанных корпусов с текстами
-def get_corpuses (dirpath: str) -> list[dict]:
+def get_corpus (dirpath: str) -> dict:
     path_list = os.listdir(dirpath)
-    text_list = []
+    corpus = {}
 
     for filepath in path_list:
         with open(f"{dirpath}/{filepath}", encoding='utf8') as txt_file:
-            text = {}
-            text[filepath.split('.')[0]] = txt_file.readlines()
-            
-            text_list.append(text)
+            corpus[filepath.split('.')[0]] = ' '.join(txt_file.readlines())
+
+    return corpus
+
+
+def clean_text (corpus_data: dict) -> dict:
+    cleaned_corpus = {}
+    punctuation = string.punctuation + '0123456789' + '\n' + '\xa0' + '«»'
     
-    return text_list
+    for name, text in corpus_data.items():
+        if name != 'PIN': continue # текст Война и Мир очень большой, на время разработки необходимая мера
+        
+        cleaned_text = text.lower()
+        
+        trans_table = str.maketrans('', '', punctuation)
+        cleaned_text = cleaned_text.translate(trans_table)
+
+        cleaned_corpus[name] = cleaned_text
+        
+    return cleaned_corpus
